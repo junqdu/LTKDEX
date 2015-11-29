7ec0dc7b8dee70ec910007ec5bf15f50b45a28c5
Jun Du <jundu@uw.edu>
2015-11-28 16:55:53 -0800

    Fix stange 404s

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,13 +10,7 @@
 angular.module('ltkdbApp')
   .controller('MainCtrl', function (Heroes) {
     this.allegianceMap = ['Shu', 'Wei', 'Wu', 'Neutral'];
-    this.heroes = Heroes.get({}, function(heroes){
-      angular.forEach(heroes, function(hero){
-        hero.name = hero.lastName + ' ' + hero.firstName;
-        delete hero.lastName;
-        delete hero.firstName;
-      });
-    });
+    this.heroes = Heroes.get();
 
     this.getLabel = function(allegiance) {
       switch(allegiance) {
